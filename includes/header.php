35692208b0135758e257df9797cdca95c163cf44
malkiaman-dev <malkiaman.profile@gmail.com>
2026-04-29 18:02:11 +0500

    typography changed to Inter and popins

--- includes/header.php
+++ includes/header.php
@@ -107,7 +107,7 @@ $productImageAlt = isset($product['product_name']) ? $product['product_name'] : 
 <link rel="stylesheet" href="<?= getBaseURL(); ?>public/assets/css/frontend/footer.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Montserrat:wght@600;700;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Poppins:wght@500;600;700;800&display=swap" rel="stylesheet">
 
 </head>
 <body>
